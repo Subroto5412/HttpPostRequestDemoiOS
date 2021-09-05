f48e25ce6f1f9a6109e6fd2f70c6b33646d5dd37
Subroto <subroto_mohonto>
2021-09-05 15:24:33 +0600

    btn add

--- HttpPostRequestDemo/ViewController.swift
+++ HttpPostRequestDemo/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
      //   ss,dcm sdc asl
     }
-
+    @IBAction func btnAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
